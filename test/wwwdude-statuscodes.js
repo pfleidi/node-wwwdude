6f56cff184a33d2b7a51731c2e84acccc316b5c8
Sven Pfleiderer <sg+github@roothausen.de>
2010-10-28 20:40:09 +0200

    Moving HTTP statuscodes to own module

---
+++ test/wwwdude-statuscodes.js
@@ -0,0 +1,57 @@
+var Helper = require('./test_helper'),
+Sys = require('sys'),
+HttpClient = require('../index'),
+Log4js = require('log4js'),
+statusCodes = require('../lib/httpcodes').codes;
+
+Log4js.addAppender(Log4js.consoleAppender());
+var logger = Log4js.getLogger('wwwdude-statuscodes');
+logger.setLevel('INFO');
+
+client = HttpClient.createClient({
+    logger: logger
+  });
+
+function _testStatus(test, verb, statusCode) {
+  logger.info('Testing Code: ' + statusCode);
+  var echoServer = Helper.echoServer(),
+  upCase = verb.replace(/del/, 'delete').toUpperCase();
+  test.expect(10);
+
+  var req = client[verb](echoServer.url + '/foo', {
+      'x-give-me-status-dude': statusCode
+    })
+  .addListener(statusCode.toString(), function (data, resp) {
+      var req = JSON.parse(data);
+      test.ok(data, 'Data must be provided');
+      test.ok(resp, 'Response must be provided');
+      test.strictEqual(req.method, upCase);
+      test.strictEqual(req.url, '/foo');
+      test.strictEqual(req.headers['user-agent'], 'node-wwwdude');
+    })
+  .addListener('error', function (data, resp) {
+      logger.info('caught error');
+    })
+  .addListener('complete', function (data, resp) {
+      var req = JSON.parse(data);
+      test.ok(data, 'Data must be provided');
+      test.ok(resp, 'Response must be provided');
+      test.strictEqual(req.method, upCase);
+      test.strictEqual(req.url, '/foo');
+      test.strictEqual(req.headers['user-agent'], 'node-wwwdude');
+      test.done();
+    }).send();
+
+  setTimeout(function () {
+      test.done();
+    }, 500);
+
+}
+
+exports.simpleTests = {
+  get: function (test) {
+    Object.keys(statusCodes).forEach(function (code) {
+        _testStatus(test, 'get', code);
+      });
+  }
+};
