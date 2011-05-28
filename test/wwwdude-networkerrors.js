7c7722744ed45ac4250ed70088fbc005936b1dea
Sven Pfleiderer <sg+github@roothausen.de>
2011-05-28 15:21:35 +0200

    Add timeout test

--- test/wwwdude-networkerrors.js
+++ test/wwwdude-networkerrors.js
@@ -32,10 +32,38 @@ exports.networkConnRefused = function (beforeExit) {
   _testClientErrs(beforeExit, 'ECONNREFUSED', 'http://localhost:63424');
 };
 
+/*
 exports.networkHostNotReachable = function (beforeExit) {
   _testClientErrs(beforeExit, 'ETIMEDOUT', 'http://127.0.22.32:63424');
 };
+*/
 
 exports.networkDomainNotFound = function (beforeExit) {
   _testClientErrs(beforeExit, 'ENOTFOUND', 'http://wrong.tld.foo.bar:63424');
+};
+
+exports.timeoutTest = function (beforeExit) {
+  var callbacks =  0;
+  var client = HttpClient.createClient({
+      timeout: 500
+    });
+  var start = new Date();
+
+  client.get('http://127.0.22.32:63424')
+  .on('error', function (err) {
+      var elapsed = new Date() - start;
+      callbacks += 1;
+      console.log(elapsed);
+      assert.ok(err);
+      assert.ok();
+    })
+  .on('complete', function () {
+      callbacks += 1;
+      throw new Error('This should not happen');
+    });
+
+
+    beforeExit(function () {
+      assert.strictEqual(callbacks, 1, 'Ensure all callbacks are called');
+    });
 };
